e32cf4d2f4dc523ec202d38eaad84ed81600f1f7
crazyah <a27328996@hotmail.com>
2020-03-20 17:59:20 +0800

    nothing commit

--- ASTRO/Unified-invoice.rb
+++ ASTRO/Unified-invoice.rb
@@ -71,6 +71,5 @@ puts is_valid_company_no?('12222539') # 印出 false
 # puts '24536806'.split('').map{ |x| x.to_i }.zip([1,2,1,2,1,2,4,1]).map{ |i| i[0] * i[1] }.map{ |i| i.to_s }.map{ |i| i[0].to_i + i[1].to_i }.sum
 # p 16.to_s
 # p ('12345678'.split(""))[6]
-
 # (serial.split(""))[6].to_i == 7
 
