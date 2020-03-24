b78578646ba9fd4207f8f3e171a4795ad579e8d7
crazyah <a27328996@hotmail.com>
2020-03-24 15:10:29 +0800

    complete count_by

--- ASTRO/count-by.rb
+++ ASTRO/count-by.rb
@@ -1,11 +1,31 @@
 def count_by(x, n)
-  n.times do 
-    x.reduce{|sum, i| i + i }
+  r = []
+  for i in (1..n)
+    i = i * x
+    r << i
   end
+  return r
 end
+
+
 
 p count_by(1, 5)    # [1, 2, 3, 4, 5]
 p count_by(2, 5)    # [2, 4, 6, 8, 10]
 p count_by(3, 5)    # [3, 6, 9, 12, 15]
 p count_by(50, 5)   # [50, 100, 150, 200, 250]
 p count_by(100, 5)  # [100, 200, 300, 400, 500]
+
+# r = []
+# x = 100
+# n = 5
+
+# for i in (1..n)
+#   i = i * x
+#   r << i
+# end
+
+# p r
+
+
+# 把 x 一個一個丟入陣列
+# 做 n 次
