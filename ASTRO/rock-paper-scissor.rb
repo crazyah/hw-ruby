82a42c2b3f4d0474703fd83c92cabefa470c88df
crazyah <a27328996@hotmail.com>
2020-04-01 09:51:34 +0800

    add rock-paper-scissor

--- ASTRO/rock-paper-scissor.rb
+++ ASTRO/rock-paper-scissor.rb
@@ -1,21 +1,8 @@
 # 實作猜拳程式
 
 def rock_paper_scissor(p1, p2)
-  if p1 == p2
-    "Draw!"
-  elsif p1 == 'scissors' && p2 == 'rock'
-    "Player 2 won!"
-  elsif p1 == 'scissors' && p2 == 'paper'
-    "Player 1 won!"
-  elsif p1 == 'rock' && p2 == 'scissors'
-    "Player 1 won!"
-  elsif p1 == 'rock' && p2 == 'paper'
-    "Player 2 won!"
-  elsif p1 == 'paper' && p2 == 'scissors'
-    "Player 2 won!"
-  elsif p1 == 'paper' && p2 == 'rock'
-    "Player 1 won!"
-  end
+  {:'rock' => 'scissors', :'scissors' => 'paper',:'paper' => 'rock'}
+  
 
 end
 
@@ -27,19 +14,25 @@ puts rock_paper_scissor('rock', 'rock')          # 印出 Draw!
 puts rock_paper_scissor('scissors', 'scissors')  # 印出 Draw!
 
 
-# if p1 == p2
-#   "Draw!"
-# elsif p1 == 'scissors' && p2 == 'rock'
-#   "Player 2 won!"
-# elsif p1 == 'scissors' && p2 == 'paper'
-#   "Player 1 won!"
-# elsif p1 == 'rock' && p2 == 'scissors'
-#   "Player 1 won!"
-# elsif p1 == 'rock' && p2 == 'paper'
-#   "Player 2 won!"
-# elsif p1 == 'paper' && p2 == 'scissors'
-#   "Player 2 won!"
-# elsif p1 == 'paper' && p2 == 'rock'
-#   "Player 1 won!"
-# end
+if p1 == p2
+  "Draw!"
 
+elsif p1 == 'scissors' && p2 == 'rock'
+  "Player 2 won!"
+
+elsif p1 == 'scissors' && p2 == 'paper'
+  "Player 1 won!"
+
+elsif p1 == 'rock' && p2 == 'scissors'
+  "Player 1 won!"
+
+elsif p1 == 'rock' && p2 == 'paper'
+  "Player 2 won!"
+
+elsif p1 == 'paper' && p2 == 'scissors'
+  "Player 2 won!"
+
+elsif p1 == 'paper' && p2 == 'rock'
+  "Player 1 won!"
+
+end
